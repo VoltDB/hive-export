b758eff353bc669730b080636c88a9799b513ef3
Stefano Santoro <ssantoro@voltdb.com>
2015-05-29 16:07:11 -0400

    make HivePartitionStream retry writer harder

--- src/main/java/org/voltdb/exportclient/hive/HivePartitionStream.java
+++ src/main/java/org/voltdb/exportclient/hive/HivePartitionStream.java
@@ -10,6 +10,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.Collection;
 import java.util.List;
 
+import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.hive.hcatalog.streaming.ConnectionError;
 import org.apache.hive.hcatalog.streaming.HiveEndPoint;
 import org.apache.hive.hcatalog.streaming.ImpersonationFailed;
@@ -20,31 +21,41 @@ import org.apache.hive.hcatalog.streaming.StreamingConnection;
 import org.apache.hive.hcatalog.streaming.StreamingException;
 import org.apache.hive.hcatalog.streaming.StrictJsonWriter;
 import org.apache.hive.hcatalog.streaming.TransactionBatch;
+import org.apache.hive.hcatalog.streaming.TransactionBatchUnAvailable;
 
 import com.google_voltpatches.common.base.Function;
 import com.google_voltpatches.common.collect.FluentIterable;
 
 public class HivePartitionStream implements Closeable {
     private final static HiveExportLogger LOG = new HiveExportLogger();
-    final static int BATCH_SIZE = 512;
+    final static int HIVE_TRANSACTION_BATCH_SIZE =
+            Integer.getInteger("HIVE_TRANSACTION_BATCH_SIZE", 64);
 
     final HiveEndPoint m_endPoint;
-    final StreamingConnection m_connection;
+    final HiveConf m_conf;
+    StreamingConnection m_connection;
     TransactionBatch m_batch;
 
     public HivePartitionStream(HiveEndPoint endPoint) {
+        m_conf = new HiveConf(HivePartitionStream.class);
+        m_conf.setVar(HiveConf.ConfVars.METASTOREURIS, endPoint.metaStoreUri);
         m_endPoint = endPoint;
+
+        connect();
+        checkBatch();
+    }
+
+    protected void connect() {
         try {
             // TODO: may need to pass user impersonation
-            m_connection = m_endPoint.newConnection(true);
+            m_connection = m_endPoint.newConnection(true, m_conf);
         } catch (ConnectionError | InvalidPartition | InvalidTable
                 | PartitionCreationFailed | ImpersonationFailed
                 | InterruptedException e) {
             String msg = "failed to connect to: %s";
-            LOG.error(msg, e, endPoint);
-            throw new HiveExportException(msg, e, endPoint);
+            LOG.error(msg, e, m_endPoint);
+            throw new HiveExportException(msg, e, m_endPoint);
         }
-        checkBatch();
     }
 
     public HiveEndPoint getEndPoint() {
@@ -54,9 +65,32 @@ public class HivePartitionStream implements Closeable {
     private void checkBatch() {
         TransactionBatch oldBatch = null;
         if (m_batch == null || m_batch.remainingTransactions() == 0)  try {
+
             oldBatch = m_batch;
-            m_batch = m_connection.fetchTransactionBatch(BATCH_SIZE, new StrictJsonWriter(m_endPoint));
+            int attemptsLeft = 3;
+            TransactionBatchUnAvailable retriedException = null;
+
+            ATTEMPT_LOOP: while (--attemptsLeft >= 0) try {
+                m_batch = m_connection.fetchTransactionBatch(
+                        HIVE_TRANSACTION_BATCH_SIZE,
+                        new StrictJsonWriter(m_endPoint)
+                        );
+                retriedException = null;
+                break ATTEMPT_LOOP;
+            } catch (TransactionBatchUnAvailable e) {
+                retriedException = e;
+                if (attemptsLeft > 0) {
+                    Thread.sleep(30);
+                } else {
+                    close(); oldBatch = null;
+                    connect();
+                }
+            }
+            if (retriedException != null) {
+                throw retriedException;
+            }
         } catch (StreamingException | InterruptedException e) {
+            close();
             String msg = "failed to get transaction batch for %s";
             LOG.error(msg, e, m_endPoint);
             throw new HiveExportException(msg, e, m_endPoint);
@@ -69,9 +103,19 @@ public class HivePartitionStream implements Closeable {
 
     @Override
     public void close() {
-        if (m_batch != null)
-            try { m_batch.close(); } catch (Exception ignoreIt) {} finally { m_batch = null; }
-        try { m_connection.close(); } catch (Exception ignoreIt) {}
+        if (m_batch != null) try {
+            m_batch.close();
+        } catch (Exception ignoreIt) {
+
+        } finally {
+            m_batch = null;
+        }
+        if (m_connection != null) try {
+            m_connection.close();
+        } catch (Exception ignoreIt) {
+        } finally {
+            m_connection = null;
+        }
     }
 
     public void write(Collection<String> jsons) {
@@ -80,26 +124,32 @@ public class HivePartitionStream implements Closeable {
         checkBatch();
         List<byte[]> messages = FluentIterable.from(jsons).transform(asBytes).toList();
 
+        int attemptsLeft = 3;
+        StreamingException retriedException = null;
         try {
-            m_batch.beginNextTransaction();
+            ATTEMPT_LOOP: while (--attemptsLeft >= 0) try {
+
+                m_batch.beginNextTransaction();
+                m_batch.write(messages);
+                m_batch.commit();
+
+                retriedException = null;
+                break ATTEMPT_LOOP;
+
+            } catch (StreamingException e) {
+                retriedException = e;
+
+                close();
+                connect();
+                checkBatch();
+            }
+            if (retriedException != null) {
+                throw retriedException;
+            }
         } catch (StreamingException | InterruptedException e) {
-            String msg = "Failed to initiate transaction against %s";
+            String msg = "Failed to write to endpoint \"%s\"";
             LOG.error(msg, e, m_endPoint);
             throw new HiveExportException(msg, e, m_endPoint);
-        }
-        try {
-            m_batch.write(messages);
-        } catch (StreamingException | InterruptedException e) {
-            String msg = "Failed to write to endpoint \"%s\" messages: %s";
-            LOG.error(msg, e, m_endPoint, jsons);
-            throw new HiveExportException(msg, e, m_endPoint, jsons);
-        }
-        try {
-            m_batch.commit();
-        } catch (StreamingException | InterruptedException e) {
-            String msg = "Failed to commit to endpoint \"%s\" messages: %s";
-            LOG.error(msg, e, m_endPoint, jsons);
-            throw new HiveExportException(msg, e, m_endPoint, jsons);
         }
     }
 
